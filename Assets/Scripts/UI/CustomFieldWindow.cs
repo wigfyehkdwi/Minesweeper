2cc429d95a882929e04ba2efa9e7f6ea7105df79
Evan2177 <evanfw12@gmail.com>
2025-05-31 13:00:11 -0400

    custom mode

--- Assets/Scripts/UI/CustomFieldWindow.cs
+++ Assets/Scripts/UI/CustomFieldWindow.cs
@@ -1,23 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 
 public class CustomFieldWindow : Window
 {
-    public TextField GridWidthField;
-    public TextField GridHeightField;
-    public TextField MineCountField;
+    public InputField GridWidthField;
+    public InputField GridHeightField;
+    public InputField MineCountField;
 
     public MineGrid Grid;
 
     public void OkButton()
     {
+        Grid.Mode = MineGrid.Modes.Custom;
         Grid.MineCount = Parse(MineCountField, 10);
         Grid.Resize(Parse(GridWidthField, 9), Parse(GridHeightField, 9));
+        Close();
     }
 
-    public int Parse(TextField field, int def)
+    public int Parse(InputField field, int def)
     {
         if (int.TryParse(field.text, out int val))
         {
